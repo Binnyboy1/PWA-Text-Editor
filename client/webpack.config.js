8ce8d2c827d487923a536927e63dd1c1baf30bdc
Binny <ibinstock@outlook.com>
2022-12-11 13:09:37 -0800

    Pushing to Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
       // ^ bundler
       new InjectManifest({
@@ -28,9 +28,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor for your convenience.',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
